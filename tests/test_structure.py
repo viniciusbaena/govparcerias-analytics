a928504bb2988ab511eec533a50a0a77d58cb755
Vinicius Baena <viniciusbaena@gmail.com>
2026-08-04 21:07:36 -0300

    feat: exportar dados oficiais por modulo

--- tests/test_structure.py
+++ tests/test_structure.py
@@ -70,6 +70,8 @@ def test_v120_publishes_official_data_and_uses_csp_safe_actions():
     assert 'data-action="open-contract"' in app
     assert 'data-action="open-proposal"' in app
     assert 'data-action="dossier-search"' in app
+    assert 'data-action="export-current"' in app
+    assert 'govparcerias-${S.view||\'dados\'}-v3.4.0-alpha.csv' in app
     assert 'dossierSearch' in app
     assert 'PNCP:' in app
     assert 'data-action="show"' in app
